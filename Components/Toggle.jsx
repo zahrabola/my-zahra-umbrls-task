39964d2c96cc8acd83200684571e71c4236d9ac7
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-02-18 16:22:19 +0000

    Update Toggle
    Taliwind CSS

--- Components/Toggle.jsx
+++ Components/Toggle.jsx
@@ -10,7 +10,7 @@ const Toggle = () => {
                 <span  class="mr-3 text-sm font-medium text-gray-900 dark:text-gray-300">Monthly</span>
                 <div class="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700  peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600"></div>
                 <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300" >Yearly</span>
-                <span class="ms-3 text-sm font-medium text-indigo-600 dark:text-gray-300 bg-indigo-50 rounded-lg px-2.5 py-2.5">Save 25%</span>
+                <span class="ms-3 text-sm font-medium text-indigo-600 dark:text-gray-300 bg-indigo-50 rounded-lg px-2 py-2">Save 25%</span>
             </label>
            </div>
         </div>
