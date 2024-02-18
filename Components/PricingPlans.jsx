10c4d18e241b7a09081caf836fc511c698aa770e
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-02-18 14:40:02 +0000

    Update
    Pricing Plan.Jsx

---
+++ Components/PricingPlans.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+
+const PricingPlans = () => {
+    return (
+        <div>
+            Pricing Plans
+        </div>
+    );
+}
+
+export default PricingPlans;
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Hero from "@/Components/Hero";
 import HeroTwo from "@/Components/HeroTwo";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
