7379b66e6fbcec03a9531f0dde4f1c2d1c72b12d
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-02-18 14:46:49 +0000

    Update Pricing

--- Components/PricingPlans.jsx
+++ Components/PricingPlans.jsx
@@ -1,20 +1,31 @@
 "use client";
-import React from 'react';
+import React from "react";
 import { dataPricingPlans } from "../DataPrice/index";
 
 const PricingPlans = () => {
-    return (
-        <div>
-            <div>
+  return (
+    <div className="max-w-screen-xl mx-auto px-4 py-3 items-center justify-content: center">
+      <div className="mx-auto max-w-7xl grid lg:grid-cols-3 lg:px-8 sm:px-6 gap-8 ">
         {dataPricingPlans.map((plan) => (
-            <div key={plan.title}>
-
-                <h3>{plan.title}</h3>
-                </div>
-        ))}
+          <div
+            key={plan.title}
+            className=" flex flex-col border border-slate-200 p-4 mt-2 rounded-2xl bg-white shadow-md p-8"
+          >
+            <h3 className="text-lg leading-5">{plan.title}</h3>
+            <div>
+              <p>
+                <span>{plan.currency}</span>
+                <span>{plan.price}</span>
+                <span>{plan.frequency}</span>
+              </p>
+              <p>{plan.description}</p>
             </div>
-        </div>
-    );
-}
+            {/* features */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default PricingPlans;
