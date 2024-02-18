8e2b565bba3016efeab0ce185f570afd452e8ca2
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-02-18 14:35:10 +0000

    Update Hero
    Tailwind Css

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -4,16 +4,15 @@ import React from 'react';
 const Hero = () => {
     return (
         <div>
-            <section>
-                <div>
-                    <div>
-                        <p>
+            <section className='py-14 relative bg-white'>
+                <div className='relative z-10 max-w-screen-xl mx-auto px-4 text-center md:px-8'>
+                    <div className='max-w-xl md:mx-auto'>
+                        <p className='text-black text-3xl font-bold text-black sm:text-5xl'>
                         Pricing & Plans 
                         </p>
-                        <p>
+                        <p className='text-black-100 mt-3'>
                         With lots of unique blocks, you can easily build a page without coding. Build your next consultancy website within few minutes.
                         </p>
-
                     </div>
                 </div>
             </section>
