eaab0efebaa5c0b10556a329cb469aafa04e6799
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-02-18 14:30:58 +0000

    Update Hero

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -1,9 +1,22 @@
+"use client";
 import React from 'react';
 
 const Hero = () => {
     return (
         <div>
-            
+            <section>
+                <div>
+                    <div>
+                        <p>
+                        Pricing & Plans 
+                        </p>
+                        <p>
+                        With lots of unique blocks, you can easily build a page without coding. Build your next consultancy website within few minutes.
+                        </p>
+
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
